7f92bae9e94cf3d9b6b54ffe6f3e1c5133776094
yann dardot <yann@donkeycode.com>
2018-07-23 18:49:42 +0200

    remove calc when no channel

--- intervals/youtube.js
+++ intervals/youtube.js
@@ -10,8 +10,11 @@ var youtubeRequest = function (guild) {
   }, (error, response, body) => {
     let result = JSON.parse(body);
     let nb = result.items[0].statistics.subscriberCount;
-    if (nb !== youtube.lastNbSubscribers && Math.floor(youtube.lastNbSubscribers/youtube.interval) <  Math.floor(nb/youtube.interval)) {
-
+    if (
+      nb !== youtube.lastNbSubscribers 
+      && Math.floor(youtube.lastNbSubscribers/youtube.interval) <  Math.floor(nb/youtube.interval)
+      && youtube.channel
+    ) {
       oldcap = youtube.getNextCap(youtube.lastNbSubscribers);
       cap = youtube.getNextCap(nb);
       if (oldcap < cap && youtube.capmessages.length > 0) {
@@ -24,8 +27,7 @@ var youtubeRequest = function (guild) {
       message = message.replace(new RegExp('%cap%', 'g'), Utils.spacer(cap));
       message = message.replace(new RegExp('%oldcap%', 'g'), Utils.spacer(oldcap));
       message = message.replace(new RegExp('%cap-total%', 'g'), Utils.spacer(cap - nb));
-      if (!youtube.channel)
-        guild.channels.get(youtube.channel).send(message);
+      guild.channels.get(youtube.channel).send(message);
     }
     if(nb !== youtube.lastNbSubscribers) {
       youtube.lastNbSubscribers = nb;
